8e7daf852f1071bcdc554493aeaa259f175936c8
ncependanm <ncependanmms@gmail.com>
2025-05-15 20:59:14 +0700

    Mengubah template login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,41 @@
 @extends('layouts.auth')
 
 @section('login')
-<div class="login-box">
+<div class="text-center">
 
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <div class="login-logo">
-            <a href="{{ url('/') }}">
-                <img src="{{ url($setting->path_logo) }}" alt="logo.png" width="100">
-            </a>
-        </div>
-
-        <form action="{{ route('login') }}" method="post" class="form-login">
-            @csrf
-            <div class="form-group has-feedback @error('email') has-error @enderror">
-                <input type="email" name="email" class="form-control" placeholder="Email" required value="{{ old('email') }}" autofocus>
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                @error('email')
-                    <span class="help-block">{{ $message }}</span>
-                @else
-                <span class="help-block with-errors"></span>
-                @enderror
-            </div>
-            <div class="form-group has-feedback @error('password') has-error @enderror">
-                <input type="password" name="password" class="form-control" placeholder="Password" required>
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                @error('password')
-                    <span class="help-block">{{ $message }}</span>
-                @else
-                    <span class="help-block with-errors"></span>
-                @enderror
-            </div>
-            <div class="row">
-                <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                        <label>
-                            <input type="checkbox"> Remember Me
-                        </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                </div>
-                <!-- /.col -->
-            </div>
-        </form>
-    </div>
-    <!-- /.login-box-body -->
+        <img src="{{ url($setting->path_logo) }}" alt="logo.png" width="100" class="logo">
+        <h1 class="text-white h3">Login</h1>
 </div>
-<!-- /.login-box -->
+<form class="mt-4" action="{{ route('login') }}" method="post">
+    @csrf
+    <div class="input-group uf-input-group input-group-lg mb-3">
+        <span class="input-group-text fa fa-user"></span>
+        <input type="text" class="form-control" name="email" placeholder="Username or Email address"
+            value="{{ old('email') }}">
+        @error('email')
+        <span class="help-block">{{ $message }}</span>
+        @else
+        <span class="help-block with-errors"></span>
+        @enderror
+    </div>
+    <div class="input-group uf-input-group input-group-lg mb-3">
+        <span class="input-group-text fa fa-lock"></span>
+        <input type="password" name="password" class="form-control" placeholder="Password">
+        @error('password')
+        <span class="help-block">{{ $message }}</span>
+        @else
+        <span class="help-block with-errors"></span>
+        @enderror
+    </div>
+    <div class="d-flex mb-3 justify-content-between">
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input uf-form-check-input" id="exampleCheck1">
+            <label class="form-check-label text-white" for="exampleCheck1">Remember Me</label>
+        </div>
+        {{-- <a href="#">Forgot password?</a> --}}
+    </div>
+    <div class="d-grid mb-4">
+        <button type="submit" class="btn uf-btn-primary btn-lg">Login</button>
+    </div>
+</form>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,55 +1,30 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name') }} | Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ url($setting->path_logo) }}" type="image/png">
-
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-2/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-2/bower_components/font-awesome/css/font-awesome.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-2/dist/css/AdminLTE.min.css') }}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-2/plugins/iCheck/square/blue.css') }}">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/uf-style.css')}}">
+    <title>Login Form Bootstrap 1 by UIFresh</title>
 </head>
-<body class="hold-transition login-page">
-    
-    @yield('login')
 
-    <!-- jQuery 3 -->
-    <script src="{{ asset('AdminLTE-2/bower_components/jquery/dist/jquery.min.js') }}"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="{{ asset('AdminLTE-2/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <!-- iCheck -->
-    <script src="{{ asset('AdminLTE-2/plugins/iCheck/icheck.min.js') }}"></script>
-    <!-- Validator -->
-    <script src="{{ asset('js/validator.min.js') }}"></script>
-    <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%' /* optional */
-            });
-        });
-        $('.form-login').validator();
-    </script>
+<body>
+    <div class="uf-form-signin">
+        @yield('login')
+
+    </div>
+
+    <!-- JavaScript -->
+
+    <!-- Separate Popper and Bootstrap JS -->
+    <script src="{{ asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
 </body>
+
 </html>
